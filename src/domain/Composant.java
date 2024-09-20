14cb61714f7e9ddcd964a3a9fd11b82eea27fee3
salekelfahim <salekelfahim@gmail.com>
2024-09-20 12:00:08 +0100

    Adding Client, Composant, Projet, MaoinOeuvre classes

--- src/domain/Composant.java
+++ src/domain/Composant.java
@@ -1,4 +1,34 @@
 package domain;
 
-public class Composant {
+import java.math.BigDecimal;
+
+public abstract class Composant {
+    private Long id;
+    private String nom;
+    private String typeComposant;
+    private BigDecimal tauxTva;
+    private Projet projet;
+
+    public Composant() {}
+
+    public Composant(String nom, String typeComposant, BigDecimal tauxTva, Projet projet) {
+        this.nom = nom;
+        this.typeComposant = typeComposant;
+        this.tauxTva = tauxTva;
+        this.projet = projet;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+    public String getTypeComposant() { return typeComposant; }
+    public void setTypeComposant(String typeComposant) { this.typeComposant = typeComposant; }
+    public BigDecimal getTauxTva() { return tauxTva; }
+    public void setTauxTva(BigDecimal tauxTva) { this.tauxTva = tauxTva; }
+    public Projet getProjet() { return projet; }
+    public void setProjet(Projet projet) { this.projet = projet; }
+
+    public abstract BigDecimal calculerCout();
 }
+
--- src/domain/MainOeuvre.java
+++ src/domain/MainOeuvre.java
@@ -1,4 +1,32 @@
 package domain;
 
-public class MainOeuvre {
+import java.math.BigDecimal;
+
+public class MainOeuvre extends Composant {
+    private BigDecimal tauxHoraire;
+    private BigDecimal heuresTravail;
+    private BigDecimal productiviteOuvrier;
+
+    // Constructors, getters, and setters
+    public MainOeuvre() {}
+
+    public MainOeuvre(String nom, BigDecimal tauxTva, Projet projet, BigDecimal tauxHoraire, BigDecimal heuresTravail, BigDecimal productiviteOuvrier) {
+        super(nom, "MainOeuvre", tauxTva, projet);
+        this.tauxHoraire = tauxHoraire;
+        this.heuresTravail = heuresTravail;
+        this.productiviteOuvrier = productiviteOuvrier;
+    }
+
+    // Getters and setters
+    public BigDecimal getTauxHoraire() { return tauxHoraire; }
+    public void setTauxHoraire(BigDecimal tauxHoraire) { this.tauxHoraire = tauxHoraire; }
+    public BigDecimal getHeuresTravail() { return heuresTravail; }
+    public void setHeuresTravail(BigDecimal heuresTravail) { this.heuresTravail = heuresTravail; }
+    public BigDecimal getProductiviteOuvrier() { return productiviteOuvrier; }
+    public void setProductiviteOuvrier(BigDecimal productiviteOuvrier) { this.productiviteOuvrier = productiviteOuvrier; }
+
+    @Override
+    public BigDecimal calculerCout() {
+        return tauxHoraire.multiply(heuresTravail).multiply(productiviteOuvrier);
+    }
 }
