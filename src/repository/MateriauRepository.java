61d03462045324197556c96735fcfe288ba5fb87
salekelfahim <salekelfahim@gmail.com>
2024-09-20 12:37:23 +0100

    Adding Connection Instence to All repoitories

--- src/repository/MateriauRepository.java
+++ src/repository/MateriauRepository.java
@@ -1,4 +1,13 @@
 package repository;
 
+import config.DataBaseConnection;
+
+import java.sql.Connection;
+
 public class MateriauRepository {
+    private Connection connection;
+
+    public MateriauRepository() {
+        this.connection = DataBaseConnection.getInstance().getConnection();
+    }
 }
--- src/repository/ProjetRepository.java
+++ src/repository/ProjetRepository.java
@@ -1,4 +1,13 @@
 package repository;
 
+import config.DataBaseConnection;
+
+import java.sql.Connection;
+
 public class ProjetRepository {
+    private Connection connection;
+
+    public ProjetRepository() {
+        this.connection = DataBaseConnection.getInstance().getConnection();
+    }
 }
