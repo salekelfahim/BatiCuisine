e0e2ec5e92da0d8424e6e7b84bc854b031c5097a
salekelfahim <salekelfahim@gmail.com>
2024-09-21 16:18:15 +0100

    Adding Projet repository

--- src/repository/interfaces/IProjetRepository.java
+++ src/repository/interfaces/IProjetRepository.java
@@ -1,4 +1,16 @@
 package repository.interfaces;
 
+
+import domain.Projet;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IProjetRepository {
+    Projet save(Projet projet);
+    Optional<Projet> findById(Long id);
+    List<Projet> findAll();
+    Projet update(Projet projet);
+    void delete(Long id);
+    List<Projet> findByClientId(Long clientId);
 }
